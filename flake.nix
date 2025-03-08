bf33bea34817d3d5633754be76b66d6cf192298b
Zachary Pfeiffer <zpfeiffer@berkeley.edu>
2025-03-08 02:04:01 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "Air Dev Shell Flake";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    esp-dev = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      debug = true;
+
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            # inputs.esp-dev.overlays.default
+            inputs.rust-overlay.overlays.default
+          ];
+          config = {};
+        };
+
+        # Define the formatter used by `nix fmt`
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rust-bin.fromRustupToolchainFile
+              ./rust-toolchain.toml)
+
+            # CLI tool for on-chip debugging and flashing of ARM chips
+            probe-rs-tools
+
+            # https://docs.esp-rs.org/book/writing-your-own-application/generate-project/esp-generate.html
+            esp-generate
+
+            # Provided by esp-dev overlay
+            # esp-idf-full
+          ];
+
+          packages = [];
+          inputsFrom = [];
+          shellHook = ''
+            # Install pre-commit hooks
+            ${config.pre-commit.installationScript}
+            echo 1>&2 "Started Air development shell"
+          '';
+        };
+
+        pre-commit = {
+          # Run pre-commit hooks as part of `nix flake check`
+          check.enable = true;
+          settings.hooks = {
+            # Nix formatter
+            alejandra.enable = true;
+
+            # Nix linter
+            statix.enable = true;
+
+            mixed-line-endings.enable = true;
+            end-of-file-fixer.enable = true;
+          };
+        };
+      };
+    });
+}
---
+++ src/bin/main.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::systimer::SystemTimer;
+use esp_hal::timer::timg::TimerGroup;
+use panic_rtt_target as _;
+
+extern crate alloc;
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    // generator version: 0.3.0
+
+    rtt_target::rtt_init_defmt!();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+
+    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
+
+    info!("Embassy initialized!");
+
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let _init = esp_wifi::init(
+        timer1.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
+}
